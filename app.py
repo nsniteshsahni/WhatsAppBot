7491f47c6426206a6c15b1537c6b2c73c3ac70f0
nsniteshsahni <nsniteshsahni.007@gmail.com>
2020-04-07 21:22:00 +0530

    Project created

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+from utils import fetch_reply
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Fetch the message
+    msg = request.form.get('Body')
+    phone_number = request.form.get('From')
+    # Create reply
+    reply = fetch_reply(msg, phone_number)
+    resp = MessagingResponse()
+    resp.message(reply)
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
